d8ff383c0b5902470d084d48b210572ac40743ee
vaniltonpaulo <V.Paulo@campus.lmu.de>
2026-04-25 16:52:55 +0200

    added glue IAM role

---
+++ infrastructure/modules/glue-job/main.tf
@@ -0,0 +1,46 @@
+resource "aws_iam_role" "glue_role" {
+  name = "${var.project_name}-glue-role-${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "glue.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "glue_service_role" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+resource "aws_iam_role_policy" "glue_s3_policy" {
+  name = "${var.project_name}-glue-s3-policy"
+  role = aws_iam_role.glue_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = ["s3:GetObject", "s3:ListBucket"]
+        Resource = [
+          "arn:aws:s3:::${var.bronze_bucket_name}",
+          "arn:aws:s3:::${var.bronze_bucket_name}/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = ["s3:PutObject", "s3:DeleteObject", "s3:ListBucket"]
+        Resource = [
+          "arn:aws:s3:::${var.silver_bucket_name}",
+          "arn:aws:s3:::${var.silver_bucket_name}/*"
+        ]
+      }
+    ]
+  })
+}
---
+++ infrastructure/modules/glue-job/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "bronze_bucket_name" {
+  type = string
+}
+
+variable "silver_bucket_name" {
+  type = string
+}
